60dce1956c1b459a3b6e59125de5d42730c279ad
Marco Rezende <rezende@espressolabs.com.br>
2021-09-13 07:23:22 -0300

    CreateCategory: adicionado estado para categoria; componente Input importado; definidos métodos para lidar com formulário

--- src/components/CreateCategory.tsx
+++ src/components/CreateCategory.tsx
@@ -1,18 +1,40 @@
+import { FormEvent, useState } from 'react';
+import { createOne, getMany } from '../pages/api/common';
 import { Container } from '../styles/components/CreateCategory';
+import { Input } from './Input';
 
 interface CreateCategoryProps {
   title?: string;
 }
 
-export const CreateCategory: React.FC<CreateCategoryProps> = ({
-  title,
-  children,
-}) => {
+export const CreateCategory: React.FC<CreateCategoryProps> = ({ title }) => {
+  const [category, setCategory] = useState<string>('');
+
+  const handleInputChange = (value: string) => {
+    setCategory(value);
+  };
+
+  const handleSubmitNewCategory = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const newCategory = {
+      name: category,
+      kind: 'Humor',
+    };
+
+    console.log('creating -->', newCategory);
+
+    await createOne({ resource: 'mood', data: newCategory });
+  };
+
   return (
-    <Container>
+    <Container onSubmit={e => handleSubmitNewCategory(e)}>
       <div>
         <h2>{title || 'Nova Categoria'}</h2>
-        {children}
+        <Input
+          placeholder="Nome"
+          onChange={e => handleInputChange(e.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </div>
     </Container>
