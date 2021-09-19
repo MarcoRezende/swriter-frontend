71a91fcf39d4b5e1be02ebfc93486dde131d161d
Marco Rezende <rezende@espressolabs.com.br>
2021-09-19 01:13:35 -0300

    CategoryForm: adicionada estilização base ao componente "CategoryForm"

--- src/styles/components/CategoryForm.ts
+++ src/styles/components/CategoryForm.ts
@@ -5,7 +5,28 @@ export const Container = styled.form`
   justify-content: center;
   align-items: center;
 
+  background: url('/bg.svg') no-repeat;
+  background-size: auto;
+  background-position: center;
+
   height: 100%;
+
+  > div {
+    background: var(--background-secondary);
+    box-shadow: 0 15px 15px -5px rgb(0 0 0 / 25%);
+    padding: 2rem;
+    border-radius: 1rem;
+    max-width: 300px;
+
+    div {
+      margin-bottom: 2rem;
+      color: var(--text-black);
+
+      h2 {
+        font-weight: 800;
+      }
+    }
+  }
 
   button {
     width: 100%;
