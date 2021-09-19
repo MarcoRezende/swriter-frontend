8af6b80570f6fa48dcbfb20fcd72450389923a82
Marco Rezende <rezende@espressolabs.com.br>
2021-09-19 01:16:03 -0300

    Input: adicionada estilização base ao input

--- src/styles/components/Input.ts
+++ src/styles/components/Input.ts
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
+
+  label {
+    position: absolute;
+    bottom: 100%;
+    background: var(--background-secondary);
+    left: 10px;
+    transform: translateY(50%);
+    padding: 0 0.2rem;
+  }
+
   input {
-    border: 0;
-    border-bottom: 2px solid;
+    border: 2px solid var(--input-blur);
+    border-radius: 0.5rem;
+    width: 100%;
+    padding: 0.5rem 1rem;
+    background: transparent;
 
     &:focus {
       outline: 0;
     }
 
     &[type='placeholder'] {
-      color: var(--input-placeholder-text);
+      color: var(--input-blur);
     }
   }
 `;
