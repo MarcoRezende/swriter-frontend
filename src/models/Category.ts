96426554e7a33a8a053c36693923e6b0b162627f
Marco Rezende <rezende@espressolabs.com.br>
2021-09-25 20:12:52 -0300

    Category: entidade atualizada

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,6 +1,8 @@
 import { BaseEntity } from '../_common/base_entity';
+import { Hint } from './Hint';
 
 export class Category extends BaseEntity<Category> {
+  hint?: Hint;
   name!: string;
   kind!: string;
 }
