ae24c00df116f1a2ce3d849693de65a7984b0b48
Marco Rezende <rezende@espressolabs.com.br>
2021-09-25 20:24:41 -0300

    _app: adicionado app provider dos contextos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app';
 import { Navbar } from '../components/Nabvar';
+import AppProvider from '../context';
 import { GlobalStyles } from '../styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AppProvider>
       <Component {...pageProps} />
       <Navbar />
       <GlobalStyles />
-    </>
+    </AppProvider>
   );
 }
 export default MyApp;
