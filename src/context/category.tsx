434c6908c1606752fa40634dbc20bf85b1e39532
Marco Rezende <rezende@espressolabs.com.br>
2021-09-25 20:10:59 -0300

    category: adicionado contexto base para categoria

---
+++ src/context/category.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext } from 'react';
+import { Category } from '../models/Category';
+import { CategoryResource } from '../pages/api/category';
+
+import { createOneBase } from '../pages/api/common';
+
+export interface CategoryAPI {
+  createOne(categoryDto: Category): Promise<Category>;
+}
+
+export const CategoryContext = createContext<CategoryAPI>({} as CategoryAPI);
+
+export const CategoryProvider: React.FC = ({ children }) => {
+  const createOne = async (categoryDto: Category): Promise<Category> => {
+    const category = new Category(categoryDto);
+
+    return (
+      (
+        await createOneBase<Category>({
+          resource: CategoryResource,
+          data: category,
+        })
+      ).data || {}
+    );
+  };
+
+  return (
+    <CategoryContext.Provider value={{ createOne }}>
+      {children}
+    </CategoryContext.Provider>
+  );
+};
+
+export const useCategory = (): CategoryAPI => {
+  const context = useContext(CategoryContext);
+
+  if (!context) {
+    throw new Error('useCategory must be within a CategoryContext');
+  }
+
+  return context;
+};
