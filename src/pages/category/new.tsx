3c9e524578c1da9b6b4f2e2455aedb1348ccbb36
Marco Rezende <rezende@espressolabs.com.br>
2021-09-25 22:37:05 -0300

    new: implementado componente Form

--- src/pages/category/new.tsx
+++ src/pages/category/new.tsx
@@ -1,13 +1,56 @@
 import Head from 'next/head';
-import { CategoryForm } from '../../components/CategoryForm';
+
+import { useForm } from 'react-hook-form';
+
+import Form from '../../components/Form';
+import { Input } from '../../components/Input';
+import { useCategory } from '../../context/category';
+import { Category } from '../../models/Category';
 
 const NewCategory: React.FC = () => {
+  const { createOne } = useCategory();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleSubmitNewCategory = async ({ name, theme }: Category) => {
+    const category: Category = {
+      name,
+      theme,
+    };
+
+    console.log('creating -->', category);
+
+    await createOne(category);
+  };
+
   return (
     <>
       <Head>
         <title>Swriter | Nova categoria</title>
       </Head>
-      <CategoryForm />
+      <Form
+        autoComplete="off"
+        onSubmit={handleSubmit(handleSubmitNewCategory)}
+        title="Nova categoria"
+      >
+        <div>
+          <Input
+            label="Nome"
+            placeholder="nova categoria"
+            register={register('name', { required: true, maxLength: 20 })}
+          />
+
+          <Input
+            label="Tema"
+            placeholder="selecione"
+            register={register('theme', { required: true, maxLength: 20 })}
+          />
+        </div>
+      </Form>
     </>
   );
 };
