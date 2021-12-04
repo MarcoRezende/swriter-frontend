ee95484d7521485de11703844cbf59a24887429b
marco <marcorezende@outlook.com.br>
2021-12-04 18:07:37 -0300

    common: adicionado método "getOneBase"

--- src/pages/api/common.ts
+++ src/pages/api/common.ts
@@ -1,13 +1,19 @@
 import { api } from '../../config/axios';
+import { RequestQueryBuilder } from '@nestjsx/crud-request';
 
 interface DTO<T> {
   resource: string;
   data?: Partial<T>;
   id?: Partial<T>;
+  params?: RequestQueryBuilder;
 }
 
 export const createOneBase = async <K>({ resource, data }: DTO<K>) => {
   return api.post<K>(resource, data);
+};
+
+export const getOneBase = async <K>({ resource, params }: DTO<K>) => {
+  return api.get<K>(`${resource}/random`, { params: params?.queryString });
 };
 
 export const getManyBase = async <K>({ resource }: DTO<K>) => {
