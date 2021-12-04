4616fef630410bcc9c637a5c8157c8a564f52201
marco <marcorezende@outlook.com.br>
2021-12-04 16:51:11 -0300

    Home: Estruturado conteudo da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,63 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Container } from '../styles/pages/Home';
+import { useCallback, useEffect, useState } from 'react';
+import { useHint } from '../context/hint';
+import { Hint } from '../models/Hint';
+import { Container, Button } from '../styles/pages/Home';
+
+import { BsArrowClockwise } from 'react-icons/bs';
 
 const Home: NextPage = () => {
+  const { randomHint, getOne } = useHint();
+  const [hint, setHint] = useState<Hint>({} as Hint);
+  const [rotate, setRotate] = useState<boolean>(false);
+  const hasCategories = !!hint?.categories?.length;
+
+  useEffect(() => {
+    setHint(randomHint);
+  }, [randomHint]);
+
+  const getRandomHint = useCallback(async () => {
+    await getOne();
+  }, [getOne]);
+
   return (
     <>
       <Head>
         <title>Swriter | Home</title>
       </Head>
       <Container>
-        <div />
+        <div>
+          <p className="sentence">{`"${hint.tip}"`}</p>
+
+          {hasCategories && (
+            <ul>
+              {hint.categories?.map(category => (
+                <li key={category.id}>{category.name}</li>
+              ))}
+            </ul>
+          )}
+
+          <div>
+            <p>{hint.author || 'Autor desconhecido'}</p>
+            <p>{hint.book || 'Origem desconhecida'}</p>
+            <p>
+              Frase gerada {hint.timesDrawn}{' '}
+              {hint.timesDrawn || 2 > 1 ? 'vezes' : 'vez'}
+            </p>
+          </div>
+        </div>
+
+        <Button
+          size="2rem"
+          $rotate={rotate}
+          color="#303030"
+          strokeWidth="0.5px"
+          onClick={() => {
+            getRandomHint();
+            setRotate(!rotate);
+          }}
+        />
       </Container>
     </>
   );
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,99 @@
-import styled from 'styled-components';
+import { BsArrowClockwise } from 'react-icons/bs';
+import styled, { css } from 'styled-components';
+
+interface ButtonProps {
+  $rotate?: boolean;
+}
 
 export const Container = styled.main`
   height: 100vh;
+  max-width: 900px;
+
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  font-family: Libre Baskerville;
+  color: #303030;
+
+  > div {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .sentence {
+      font-style: italic;
+      font-size: 1.4rem;
+    }
+
+    ul {
+      list-style: none;
+
+      display: flex;
+      flex-wrap: wrap;
+      gap: 0.5rem;
+
+      margin: 0.5rem 0 1rem;
+
+      li {
+        border: 1px solid #303030;
+        border-radius: 1rem;
+        padding: 0.5rem 1rem;
+
+        font-size: 0.9rem;
+      }
+    }
+
+    div:first-of-type {
+      margin-top: 1rem;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      p:first-child {
+        margin-bottom: 0.3rem;
+      }
+
+      p:last-child {
+        margin: 1.5rem 0;
+        font-size: 1.1rem;
+      }
+
+      p:nth-child(-2n + 2) {
+        &::after {
+          content: '';
+          border-bottom: 1px solid #303030;
+          width: 50%;
+
+          display: block;
+          margin: 0.4rem auto 0;
+        }
+      }
+    }
+  }
+`;
+
+export const Button = styled(BsArrowClockwise)<ButtonProps>`
+  margin: auto 0 2rem;
+  background: none;
+  border: 0;
+
+  cursor: pointer;
+
+  transition: transform 0.4s;
+
+  ${({ $rotate }) => {
+    return (
+      $rotate &&
+      css`
+        transform: rotate(360deg);
+      `
+    );
+  }}
 `;
