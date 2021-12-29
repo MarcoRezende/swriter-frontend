14d90a85d47ce273832c0cdd89c671ba7949a77f
marco <marcorezende@outlook.com.br>
2021-12-29 15:09:12 -0300

    _document: adicionada fonte Poppins

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
